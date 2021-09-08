83851ff46fccc3d3d4632308d58f1b0f3bf6c83d
the-macson <ak9991927952@gmail.com>
2021-09-08 20:48:49 +0530

    added code

--- 450-DSA-Quiz/Array & String/Quiz_01.cpp
+++ 450-DSA-Quiz/Array & String/Quiz_01.cpp
@@ -9,7 +9,16 @@ vector<int> sortedSquares(vector<int> &nums){
     sort(nums.begin(),nums.end());
     return nums;
     // method 2
-    
+    vector<int> res(nums.size());
+    int l = 0, r = nums.size() - 1;
+    for (int k = nums.size() - 1; k >= 0; k--)
+    {
+        if (abs(nums[r]) > abs(nums[l]))
+            res[k] = nums[r] * nums[r--];
+        else
+            res[k] = nums[l] * nums[l++];
+    }
+    return res;
 }
 int main(){
     int n,x;
